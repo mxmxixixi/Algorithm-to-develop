4ac5222934e04de0814f7cb4a3471ce947a64d0f
miaowenyu <mxm17790077530@163.com>
2021-01-14 20:30:57 +0800

    (feat):快速排序-215

--- README.md
+++ README.md
@@ -245,3 +245,4 @@
 
 - 题目
 
+  - #### [215. 数组中的第K个最大元素](https://leetcode-cn.com/problems/kth-largest-element-in-an-array/)
--- 排序/Code1-215.ts
+++ 排序/Code1-215.ts
@@ -5,48 +5,29 @@
  * 第一种解题思路
  * 
  */
-function validIPAddress(IP: string): string {
-    if((/\./).test(IP)){
-		const found =IP.split(".");
-        const regex = /^\d+$/;
-        if(found.length != 4){
-            return 'Neither'
-        }
-		for(let i=0;i<found.length;i++){
-            let v= found[i]
-            if((v.length>=2 && v[0] === '0') || Number(v)<0 || Number(v)>255 ||v===''||  !regex.test(v)){
-				return 'Neither'
-			}
-        }
-        return 'IPv4'
-    }else if((/\:/).test(IP)){
-		const found = IP.split(":");
-        const regex = '0123456789abcdefABCDEF';
-		let result = 0;
-        if(found.length!=8){
-            return 'Neither'
-        }
-		for(let i=0;i<found.length;i++){
-            let v = found[i]
-            if(v === '' || v.length>4){
-				return 'Neither'
-			}else{
-                for (let i=0;i<v.length;i++) {
-                    if (regex.indexOf(v[i]) == -1){
-                        return 'Neither'
-                    }
+function findKthLargest(nums: number[], k: number): number {
+    const quickSort = (left:number,right:number) => {
+        const standard = nums[left];
+        while(left<right){
+            if(nums[right]<standard){
+                nums[left] = nums[right];
+                left++
+                if(nums[left]>standard){
+                    nums[right] = nums[left];
+                }else{
+                    left++
                 }
-			}
+            }else{
+                right--
+            }
         }
-        return 'IPv6'
-	}
-    return 'Neither'
+        nums[left] = standard
+    }
+    quickSort(0,5);
+    console.log("222",nums)
+    return 1
 };
-console.log("validIPAddress",validIPAddress("172.16.254.1"))
-console.log("validIPAddress",validIPAddress("2001:0db8:85a3:0:0:8A2E:0370:7334"))
-console.log("validIPAddress",validIPAddress("256.256.256.256"))
-console.log("validIPAddress",validIPAddress("2001:0db8:85a3:0:0:8A2E:0370:7334:"))
-console.log("validIPAddress",validIPAddress("1e1.4.5.6"))
-
+console.log("findKthLargest",findKthLargest([3,2,1,5,6,4],2))
+console.log("findKthLargest",findKthLargest([3,2,3,1,2,4,5,5,6],4))
 
 

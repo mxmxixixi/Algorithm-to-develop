1319cbca0d4f33dc19e5b504f8b10db8a1689470
miaowenyu <mxm17790077530@163.com>
2021-01-24 22:30:22 +0800

    feat: 排序-直接插入排序-147

--- 排序/直接插入排序/Code2-147.ts
+++ 排序/直接插入排序/Code2-147.ts
@@ -1,4 +1,9 @@
 /**
+ * 按照题描述使用直接插入排序
+ * 对于单链表的排序，只能是单向的寻找，所以在每次插入时只能从第一个元素开始寻找
+ * 为了提升性能，可以记录每次寻找的最后一个元素，如果要插入的组值大于最后一个元素，则可以直接插入
+ */
+/**
  * Definition for singly-linked list.
  * class ListNode {
  *     val: number
@@ -9,33 +14,27 @@
  *     }
  * }
  */
-
 function insertionSortList(head: ListNode | null): ListNode | null {
     if(head === null){
         return head
     }
-    const _head:ListNode = null
-    const insertionSort = (node:ListNode) => {
-        if(node === null){
-            return
-        }
-        if(node.val <=head.val){
-            node.next = head
+    let _head:ListNode = new ListNode(0),lastSorted = head,curr = head.next;
+    _head.next = head
+    while(curr != null){
+        if(lastSorted.val <=curr.val){
+            lastSorted = lastSorted.next
         }else{
-            let nodeCompare = head.next
-            while(nodeCompare != null){
-                if(node.val <=nodeCompare.val){
-                    node.next = nodeCompare
-                    break
-                }else{
-                    nodeCompare = nodeCompare.next
-                }
+            let nodeCompare = _head
+             while (nodeCompare.next.val <= curr.val) {
+                nodeCompare = nodeCompare.next;
             }
+            lastSorted.next = curr.next;
+            curr.next = nodeCompare.next;
+            nodeCompare.next = curr;
         }
-        insertionSort(node.next)
+        curr = lastSorted.next;
     }
-    insertionSort(head.next)
-    return head
+    return _head.next
 };
 // console.log("insertionSortList",insertionSortList(12,15,2))
 // console.log("insertionSortList",insertionSortList(1,1,1))
